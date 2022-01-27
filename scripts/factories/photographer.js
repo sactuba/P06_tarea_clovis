71d1a1ab695d9b7eab0a0affd6bf8ed9a899cdee
sactuba <taloamn85@gmail.com>
2022-01-27 17:43:10 +0100

    Mise a jour test

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,22 +1,16 @@
 function photographerFactory(photographer) {
-    const { name, portrait, id, title, country, city, tagline, price } = photographer;
+    const { name, portrait, id, country, city, tagline, price } = photographer;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-/*      const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2); */
+
         const article =
         `<article>
         <a href="/photographer.html?id=${id}">
         <img src="${picture}"></img>
         </a>
-        <h2>${title}</h2>
+        <h2>${name}</h2>
         <span>${country}, ${city}</span>
         <p>${tagline}</p>
         <p>${price} €/jour</p>
@@ -25,5 +19,5 @@ function photographerFactory(photographer) {
         `
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture,id, country, city, tagline, price, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,17 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function photographerData() {
+    const url = new URLSearchParams(document.location.search);
+    const id = url.get('id');
+    let response = await fetch('../../data/photographers.json');
+    let data = await response.json();
+    
+    //console.log(data);
+
+    //filtrer les données media par rapport a l'id des photographe
+    const photographerData = data.media.filter(photographer => photographer.id == id)[0];
+    const photographerMedias = data.media.filter(media => media.photographerData == photographerData.id);
+    console.log(photographerMedias);
+}
+
+photographerData();
